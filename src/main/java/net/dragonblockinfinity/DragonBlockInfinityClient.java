ec6857b31549323b600fa5b7c84de75dc3556b83
bernardopfgdg2011-pn <bernardopfgdg2011@gmail.com>
2026-06-24 22:09:47 +0000

    gradlew build funfando

---
+++ src/main/java/net/dragonblockinfinity/DragonBlockInfinityClient.java
@@ -0,0 +1,10 @@
+package net.dragonblockinfinity;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DragonBlockInfinityClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client initialization logic goes here.
+    }
+}
